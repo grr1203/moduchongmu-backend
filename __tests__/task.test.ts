bfac18bdae47bc4c6522e8b5823d470d9b5ed8e0
grr1203 <gyrms9412@naver.com>
2024-06-13 18:46:47 +0900

    친구 목록 조회 API 테스트 작성

--- __tests__/task.test.ts
+++ __tests__/task.test.ts
@@ -2,6 +2,7 @@ import { createPublicLambdaEvent, privateFunctionTest } from './testUtil';
 import { handler as getTest } from '../src/ts/test/get';
 import { handler as postFriend } from '../src/ts/friend/post';
 import { handler as getFriendSearch } from '../src/ts/friend/search/get';
+import { handler as getFriend } from '../src/ts/friend/get';
 
 describe('ModuChongmu test', () => {
   test('GET test', async () => {
@@ -20,6 +21,12 @@ describe('ModuChongmu test', () => {
   test('POST friend', async () => {
     const parameters = { newFriendName: 'hyofriend' };
     const res = await privateFunctionTest(postFriend, parameters);
+    expect(res).toHaveProperty('statusCode', 200);
+  });
+
+  test.only('GET friend', async () => {
+    const parameters = {};
+    const res = await privateFunctionTest(getFriend, parameters);
     expect(res).toHaveProperty('statusCode', 200);
   });
 });
