bfb83862aeba31935ddbb74da91d048e44f40816
grr1203 <gyrms9412@naver.com>
2024-06-13 18:38:52 +0900

    친구 목록 조회 API 구현

---
+++ src/ts/lib/user.ts
@@ -0,0 +1,6 @@
+import mysqlUtil from './mysqlUtil';
+
+export async function idxToUser(idxArray: number[]) {
+  const userArray = idxArray.map(async (idx) => await mysqlUtil.getOne('tb_user', [], { idx }));
+  return await Promise.all(userArray);
+}
