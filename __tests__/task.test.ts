d69eb77f555f67562a91a81e30bdcbef1d4caf2e
grr1203 <gyrms9412@naver.com>
2024-06-14 13:43:33 +0900

    여행 방 생성 API 테스트 작성

--- __tests__/task.test.ts
+++ __tests__/task.test.ts
@@ -3,6 +3,7 @@ import { handler as getTest } from '../src/ts/test/get';
 import { handler as postFriend } from '../src/ts/friend/post';
 import { handler as getFriendSearch } from '../src/ts/friend/search/get';
 import { handler as getFriend } from '../src/ts/friend/get';
+import { handler as postTravel } from '../src/ts/travel/post';
 
 describe('ModuChongmu test', () => {
   test('GET test', async () => {
@@ -24,9 +25,21 @@ describe('ModuChongmu test', () => {
     expect(res).toHaveProperty('statusCode', 200);
   });
 
-  test.only('GET friend', async () => {
+  test('GET friend', async () => {
     const parameters = {};
     const res = await privateFunctionTest(getFriend, parameters);
+    expect(res).toHaveProperty('statusCode', 200);
+  });
+
+  test.only('POST travel', async () => {
+    const parameters = {
+      travelName: 'templestay',
+      destination: 'bukhansan',
+      startDate: '2024-06-14',
+      endDate: '2024-06-22',
+      memo: '메모 크크',
+    };
+    const res = await privateFunctionTest(postTravel, parameters);
     expect(res).toHaveProperty('statusCode', 200);
   });
 });
