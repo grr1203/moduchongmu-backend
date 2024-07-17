367b8caf6e8a12d7d9409e10e7181a0a8bedad64
grr1203 <gyrms9412@naver.com>
2024-07-17 13:38:47 +0900

    여행 초대 수락 API 구현

---
+++ src/ts/travel/invite/put.ts
@@ -0,0 +1,22 @@
+import { APIGatewayProxyEventV2WithLambdaAuthorizer } from 'aws-lambda';
+import mysqlUtil from '../../lib/mysqlUtil';
+import { FromSchema } from 'json-schema-to-ts';
+
+const parameter = {
+  type: 'object',
+  properties: {
+    travelUid: { type: 'string' },
+  },
+  required: [],
+} as const;
+
+export const handler = async (event: APIGatewayProxyEventV2WithLambdaAuthorizer<{ [key: string]: any }>) => {
+  console.log('[event]', event);
+  const { travelUid } = JSON.parse(event.body) as FromSchema<typeof parameter>;
+  const userIdx = event.requestContext.authorizer.lambda.idx;
+
+  const travel = await mysqlUtil.getOne('tb_travel', [], { uid: travelUid });
+  await mysqlUtil.update('tb_travel_member', { active: true }, { travelIdx: travel.idx, userIdx });
+
+  return { statusCode: 200, body: JSON.stringify({}) };
+};
