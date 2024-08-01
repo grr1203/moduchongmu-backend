82bdb495c3684b975ba2211d9ba9e1c93e94ee0a
grr1203 <gyrms9412@naver.com>
2024-08-01 13:00:46 +0900

    초대 링크에서 호출할 여행 방 참여 API 구현 및 리팩토링

--- src/ts/travel/invite/post.ts
+++ src/ts/travel/invite/post.ts
@@ -6,42 +6,31 @@ const parameter = {
   type: 'object',
   properties: {
     travelUid: { type: 'string' },
-    memberName: { type: 'string' },
   },
-  required: ['travelUid', 'memberName'],
+  required: ['travelUid'],
 } as const;
 
+// 초대 링크에서 호출되는 API
 export const handler = async (event: APIGatewayProxyEventV2WithLambdaAuthorizer<{ [key: string]: any }>) => {
   console.log('[event]', event);
-  const { travelUid, memberName } = JSON.parse(event.body) as FromSchema<typeof parameter>;
+  const { travelUid } = JSON.parse(event.body) as FromSchema<typeof parameter>;
+  const userIdx = event.requestContext.authorizer.lambda.idx;
 
   const travel = await mysqlUtil.getOne('tb_travel', [], { uid: travelUid });
-  const member = await mysqlUtil.getOne('tb_user', [], { userName: memberName });
+  const travelMemberIdxList = (await mysqlUtil.getMany('tb_travel_member', ['userIdx'], { travelIdx: travel.idx })).map(
+    (member) => member.userIdx
+  );
 
-  // 1. 가입한 친구
-  if (member) {
-    // 이미 초대한 경우 (수락 여부 상관없이)
-    const isExist = await mysqlUtil.getOne('tb_travel_member', [], {
-      travelIdx: travel.idx,
-      memberName,
-      userIdx: member.idx,
-    });
-    if (isExist) return { statusCode: 200, body: JSON.stringify({ code: 'Already_Invited' }) };
+  // 여행 방에 참여
+  await mysqlUtil.create('tb_travel_member', { travelIdx: travel.idx, userIdx });
 
-    // 초대
-    await mysqlUtil.create('tb_travel_member', {
-      travelIdx: travel.idx,
-      memberName,
-      userIdx: member.idx,
-      active: false,
-    });
+  // 여행 방 참여시 방 유저들끼리 서로 친구로 등록 - tb_friend_mapping에 쌍방으로 등록
+  for (const memberIdx of travelMemberIdxList) {
+    const isExist = await mysqlUtil.getOne('tb_friend_mapping', [], { userIdx, friendIdx: memberIdx });
+    if (!isExist) await mysqlUtil.create('tb_friend_mapping', { userIdx, friendIdx: memberIdx });
+    const isExist2 = await mysqlUtil.getOne('tb_friend_mapping', [], { userIdx: memberIdx, friendIdx: userIdx });
+    if (!isExist2) await mysqlUtil.create('tb_friend_mapping', { userIdx: memberIdx, friendIdx: userIdx });
   }
-
-  // todo: 2. 가입안한 애인데 초대받고 가입할 애
-  else {
-  }
-
-  // todo: 3. 가입안했고 앞으로도 안할 애
 
   return { statusCode: 200, body: JSON.stringify({}) };
 };
